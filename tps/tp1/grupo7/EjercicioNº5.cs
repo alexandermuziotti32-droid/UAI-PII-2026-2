5379a924f66f5c2d4092a3d449850cd8ebeefc18
WillTorres23 <watl230300@gmail.com>
2026-08-24 13:05:25 -0300

    Ejercicio Nº5
    subo el ejercicio numero 5 que habla sobre la superficie de un rectangulo!

---
+++ tps/tp1/grupo7/EjercicioNº5.cs
@@ -0,0 +1,22 @@
+﻿/* Ejercicio Nº5 Dados los datos necesarios de un Rectángulo calcular la superficie.*/
+
+using System;
+namespace Proyecto
+{
+    class EjercicioNº5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese la base de un rectangulo: ");
+            double baseRectangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("ingrese la altura del rectangulo");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+            double superficie = baseRectangulo * altura;
+
+            Console.WriteLine($"La superficie del rectangulo es: {superficie}");
+        }
+    }
+
+}
